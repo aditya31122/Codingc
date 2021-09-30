58f2f9c5f34a5e6e15702c499c98ba162b23722f
Aditya singh badhwar <adityabadhwar32@gmail.com>
2021-09-30 22:33:15 +0530

    Add files via upload

---
+++ Assignment1.c
@@ -0,0 +1,33 @@
+ #include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+int main()
+{
+    int n, count;
+    float avg, d ,sum=0;
+    float list [100];
+    
+    printf("\n numbers to be averaged ?");
+    scanf("%d", &n);
+    
+    
+    for(count=0 ; count<n ; ++count);
+    {
+    printf("i= %d x=",count+1 );
+    scanf("%f" , &list[count]);
+    sum +=list[count];
+    }
+    
+    avg=sum/n;
+    printf("\n average = %5.2f", avg);
+    
+    for(count =0 ; count<n; ++count)
+    {
+        d=list[count] -avg;
+        printf("i= %d x = %5.2f d = %5.2f\n", count + 1, list[count],d);
+    }
+    
+    
+    return 0;
+}
